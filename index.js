1c5f12ceacbfcdc3bb59b20e0256c806031f90cc
José Flavio Xavier Mijangos Vasquez <jmijangos-2020303@kinal.edu.gt>
2022-07-13 12:29:39 -0600

    Mongo Atlas

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = require('./app');
 const control = require("./src/controllers/Usuario.controller")
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Hoteles', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect('mongodb+srv://gestor_hoteles:123@cluster0.zierk.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Se ha conectado correctamente a la base de datos.');
 
     control.RegistrarAd();
 
-    app.listen(3000, function (){
+    app.listen(process.env.PORT || 3000, function (){
         console.log("Hoteles, esta corriendo correctamente en el puerto 3000");
     });
 
